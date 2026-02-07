49ed5e300efa052c0799760943f8667e6607a832
Puthirith <c.puthirith01@gmail.com>
2026-02-07 18:09:48 +0700

    adjust for grading criteria

--- LittleLemon/LittleLemon/urls.py
+++ LittleLemon/LittleLemon/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls.authtoken')),
     path('auth/', include('djoser.urls')),
-    path('', include('LittleLemonAPI.urls')),
+    path('api', include('LittleLemonAPI.urls')),
 ]
